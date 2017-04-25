5b155940ad3fd0f79217fa48309a3c9f3e2d7987
Lucas Novaes <lnovaes@icasei.com.br>
2017-04-25 13:15:19 -0300

    Starting modeling

--- Hummingbird_movies/Model/Movie.swift
+++ Hummingbird_movies/Model/Movie.swift
@@ -10,22 +10,14 @@ import Foundation
 
 struct Movie{
     
+    var title : String
+    var release_date : Date
+    var overview : String
+    var poster_path : URL
     
-//    Important properties to show in the search results are the movie title (title), the year
-//    of release (year), the overview (overview) and a picture
-//    - title
-    // -year
-    // overview
-    // picture
     
-//    var title : String
-//    var release_date : Date
-//    var overview : String
-//    var poster_path : URL
-//    
-//    convenience init() {
-//        self.init()
-//    }
-
-
+    init() {
+        
+    }
+    
 }
